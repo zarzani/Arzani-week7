bd2ae1c07eb065846e5f355778ad18f235f825cb
zahra <zahraarzani@gmail.com>
2023-06-06 20:34:33 +0330

    making API link

--- weather- Arzani/App.js
+++ weather- Arzani/App.js
@@ -8,3 +8,12 @@ let minutes = now.getMinutes();
 let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 let month = months[now.getMonth()];
 h3.innerHTML = (day)+ " "+ (hour)+ ":"+ (minutes)+" " +(month)+" "+ (date);
+
+function displayTemperature(response){
+    console.log(response.data);
+}
+let apiKey = "34f95b5e87d4683b0836302b1b590869";
+let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric";
+axios.get(apiUrl).then(displayTemperature);
+
+
