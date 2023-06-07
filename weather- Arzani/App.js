fba4fc4c187caa13d8dccffd1cde94f60aff3a0d
zahra <zahraarzani@gmail.com>
2023-06-07 12:37:48 +0330

    getting data from ipa

--- weather- Arzani/App.js
+++ weather- Arzani/App.js
@@ -9,8 +9,17 @@ let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oc
 let month = months[now.getMonth()];
 h3.innerHTML = (day)+ " "+ (hour)+ ":"+ (minutes)+" " +(month)+" "+ (date);
 
-function displayTemperature(response){
+function displayTemperature(response) {
     console.log(response.data);
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round (response.data.main.temp);
+    let cityElement = document.querySelector("#city")
+    cityElement.innerHTML = (response.data.name);
+    let discriptionElement = document.querySelector("#discription");
+    discriptionElement.innerHTML = response.data.weather[0].description;
+    
+
+
 }
 let apiKey = "34f95b5e87d4683b0836302b1b590869";
 let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric";
