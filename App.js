86dde6634238ac2f473e2af0cc677af9a8bba55e
zahra <zahraarzani@gmail.com>
2023-06-07 21:47:58 +0330

    cannot do it

---
+++ App.js
@@ -0,0 +1,31 @@
+let now = new Date();
+let h3 = document.querySelector("h3");
+let date = now.getDate();
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[now.getDay()];
+let hour = now.getHours();
+let minutes = now.getMinutes();
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+let month = months[now.getMonth()];
+h3.innerHTML = (day)+ " "+ (hour)+ ":"+ (minutes)+" " +(month)+" "+ (date);
+
+function displayTemperature(response) {
+    console.log(response.data);
+
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round (response.data.main.temp);
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = (response.data.main.name);
+    let discriptionElement = document.querySelector("#discription");
+    discriptionElement.innerHTML = (response.data.weather[0].main.description);
+    let windElement = document.querySelector("#speed");
+    windElement.innerHTML = (response.data.wind.speed);
+
+
+
+}
+let apiKey = "34f95b5e87d4683b0836302b1b590869";
+let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=34f95b5e87d4683b0836302b1b590869&units=metric";
+axios.get(apiUrl).then(displayTemperature);
+
+
